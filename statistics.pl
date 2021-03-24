5d70280ea7a9de6ec9c5a5a27e15e4665ca597be
Paul Young <paulmarkyoung0@gmail.com>
2021-03-24 17:12:45 -0500

    first commit

---
+++ statistics.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+$inputFile = 'input.txt';
+$outputFile = 'output.txt';
+open($fh, '<', $inputFile);
+$data = do { local $/; <$fh> };
+close $fh;
+open($fh, '>', $outputFile);
+
+@words = split(' ', $data);
+%wordStats;
+foreach (@words) {
+  $wordStats{$_}++;
+}
+print {$fh} "Quantity of each word:\n";
+foreach (keys %wordStats){
+  print {$fh} "$_: $wordStats{$_}\n";
+}
+
+#Additional Features to be added
+# $charCount = length($data);
+# print {$fh} "Number of characters is $charCount\n";
+
+# @lines = split('\n', $data, -1);
+# $lineCount = @lines;
+# print {$fh} "Number of lines = $lineCount\n";
+
+# close $fh;
